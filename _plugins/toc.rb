590bb5fb3c19aedc9828f89a3c4da506565d1052
Egor Chiglintsev <wanderw@yandex-team.ru>
2016-03-27 13:17:02 +0600

    Add << and >> to navigation

--- _plugins/toc.rb
+++ _plugins/toc.rb
@@ -3,22 +3,9 @@ module TocFilter
     input = Kramdown::Document.new(input).to_html
 
     output = ""
-    # output = '<ul class="toc">'
-
-    current_level = nil
 
     input.scan(/<(h(2))(?:>|\s+(.*?)>)([^<]*)<\/\1\s*>/mi).each do |entry|
       level = entry[1].to_i
-
-      # unless current_level == nil
-      #   if level > current_level
-      #     output << '<ul>'
-      #   elsif level < current_level
-      #     output << '</ul>'
-      #   end
-      # end
-
-      current_level = level
 
       id = (entry[2][/^id=(['"])(.*)\1$/, 2] rescue nil)
       title = entry[3].gsub(/<(\w*).*?>(.*?)<\/\1\s*>/m, '\2').strip
@@ -31,11 +18,6 @@ module TocFilter
       end
 
     end
-
-    # while current_level > 0
-    #   output << '</ul>'
-    #   current_level -= 1
-    # end
 
     output
   end
